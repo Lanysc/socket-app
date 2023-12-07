b81ad564f453c55474ae92ab4de83b3fa5ca4ddf
JoojVitor <joaovitordossantoscouto@gmail.com>
2023-12-07 15:01:38 -0300

    Corrige erros de digitação

--- front/src/Home.js
+++ front/src/Home.js
@@ -7,19 +7,19 @@ function Home(props) {
       <form action="chat">
         <div className="content">
             <h1>Cola Chat</h1>
-            <h3>Ajudando você em suas provas diarias</h3>
+            <h3>Ajudando você em suas provas diárias</h3>
             <div className="row">
                 <select name="select_room" id="select_room">
                     <option value="-1"> Selecione a Sala</option>
                     <option value="Redes 1"> Redes 1</option>
                     <option value="Redes 2"> Redes 2</option>
                     <option value="AI"> AI </option>
-                    <option value="Sistamas Multimidia"> Sistamas Multimidia </option>
+                    <option value="Sistamas Multimidia"> Sistemas Multimídia </option>
                 </select>
             </div>
 
             <div className="row">
-                <label className="form-label"> Digite Seu usuário</label>
+                <label className="form-label"> Digite seu nome de usuário</label>
                 <input type="text" name="username" placeholder="Nome do Usuário" />
             </div>
 
